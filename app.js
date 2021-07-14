3f5971dda3b8538d36bf60b80d2cbfef425b23c7
Ben Koshy <77627889+BenjiCoder02@users.noreply.github.com>
2021-07-14 00:07:24 -0400

    Improving readability

--- app.js
+++ app.js
@@ -1,34 +1,21 @@
+   //Query selectors
+
     let bar = document.querySelector(".fa-bars");
     let x = document.querySelector("#navbar-elements");
     let button = document.getElementById("toggle"), count = 0;
+
+//Menu button animation
     button.onclick = 
     function dropdown() {
         bar.style.transition = "all 0.8s ease"
         bar.style.transform = "rotate(90deg)"
         bar.className = "fas fa-times";
         x.classList.toggle("navbar-elements-show")
-       
-        
-       
-     
-        
-    
         
         count += 1;
         if (count % 2 == 0) {
             bar.style.transition = "all 0.8s ease"
             bar.style.transform = "rotate(-180deg)";
-            bar.className = "fas fa-bars";
-           
-          
-          
-
-      
-            
-          
-            
+            bar.className = "fas fa-bars";    
         }
-    
-   
 }
-console.log(count)
